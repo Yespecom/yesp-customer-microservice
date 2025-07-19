3a2ea62ff20fbfd8ebc9fa1f49a515e0c3cd4a21
Yespecom <yespecomstudio@gmail.com>
2025-07-19 23:25:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const cors = require("cors") // Import the cors package
+
 dotenv.config() // Load environment variables from .env (or Vercel config)
 
 const customerRoutes = require("./routes/customerRoutes")
 const { connectMainDB } = require("./config/db") // Import connectMainDB
 
 const app = express()
+
+app.use(cors()) // Use cors middleware to enable CORS for all routes
 app.use(express.json()) // For parsing application/json request bodies
 
 // Connect to the main database when the server starts
